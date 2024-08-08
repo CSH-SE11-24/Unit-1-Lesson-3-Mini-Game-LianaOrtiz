f88e5546ba4acfc2f730d3ebc20b2013b442650e
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:25:56 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// Get started!
+let name = prompt("What's your name?");
+
+let num = prompt("How old are you?")
+let age = parseInt(num)
+
+let drakeFan = prompt("Do you consider yourself a Drake Fan, " + name, "?")
+
+let drakeAlbum = prompt("Choose your favorite Drake album out of the 3, (Scorpion, Take Care, Certified Lover Boy)")
+
+console.log(`Aiiii, ${name}, your favorite Drake album is ${drakeAlbum} ?` )
+
+let drakeBday = prompt("When is Drake's birthday?")
+console.log("If you said Drake's Bday was October 24th, you were right!")
